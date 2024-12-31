d9b3f83c543dd93e3bab1545fcf5f23123a1ddee
Vishnu <kmvishnu89@gmail.com>
2024-12-31 21:23:00 +0530

    Add files via upload

---
+++ CENTER/centerHome.php
@@ -0,0 +1,337 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+-->
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CenterHome -  Bootstrap Responsive Template | Home :: W3layouts</title>
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+</head>
+
+<body>
+    <!--/Header-->
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light stroke py-lg-0">
+                <h1><a class="navbar-brand" href="index.html">
+                        COMPUTISM<span class="sub-color"></span>
+                    </a></h1>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav mx-lg-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="centerHome.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="addproduct.php">Add Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="viewBookings.php">Product Bookings</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="AddService.php">Add services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="cerviceBookings.php"> service Bookings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../login.php">Logout</a>
+                        </li>
+                       
+                    </ul>
+                    <!--/search-->
+                    <button id="trigger-overlay" class="searchw3-icon me-xl-4 me-lg-3" type="button"><i class="fas fa-search"></i></button>
+                    <!-- open/close -->
+                    <div class="overlay overlay-slidedown">
+                        <button type="button" class="overlay-close"><i class="fas fa-times"></i></button>
+                        <nav class="w3l-formhny">
+                            <h5 class="mb-3">Search here</h5>
+                            <form action="#" method="GET" class="d-sm-flex search-header">
+                                <input class="form-control me-2" type="search" placeholder="Search here..." aria-label="Search" required>
+                                <button class="btn btn-style btn-primary" type="submit">Search</button>
+                            </form>
+                        </nav>
+                    </div>
+                    <!--//search-->
+                </div>
+                <!-- toggle switch for light and dark theme -->
+                <div class="mobile-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox">
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
+                        </div>
+                    </nav>
+                </div>
+                <!-- //toggle switch for light and dark theme -->
+            </nav>
+        </div>
+    </header>
+    <section class="w3l-main-slider banner-slider" id="home">
+        <div class="owl-one owl-carousel owl-theme">
+            <div class="item">
+                <div class="slider-info banner-view banner-top1">
+                    <div class="container">
+                        <div class="banner-info header-hero-19">
+                            <!-- <p class="w3hny-tag">Real Estate is our life</p> -->
+                            <h3 class="title-hero-19">Unleash The Beast : Custom PC Creation Awaits</h3>
+                            <a href="about.html" class="btn btn-style btn-primary mt-4">Read More <i class="fas fa-angle-double-right ms-2"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </section>
+    <!-- //main-slider -->
+    <!--/w3l-properties-->
+    <section class="w3l-witemshny-main py-5">
+        <div class="container py-md-4">
+            <!--/row-1-->
+            <div class="witemshny-grids row">
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        <a href="#img" class="d-block zoom"><img src="assets/images/d1.jpg" alt="" class="img-fluid news-image"></a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                    <h4 class="gdnhy-1 mt-4"><a href="#img"></a>
+                    </h4>
+                </div>
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        <a href="#img" class="d-block zoom"><img src="assets/images/d2.jpg" alt="" class="img-fluid news-image"></a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                    <h4 class="gdnhy-1 mt-4"><a href="#img"></a>
+                    </h4>
+                </div>
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        <a href="#img" class="d-block zoom"><img src="assets/images/d3.jpg" alt="" class="img-fluid news-image"></a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                    <h4 class="gdnhy-1 mt-4"><a href="#img"></a>
+                    </h4>
+                </div>
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        <a href="#img" class="d-block zoom"><img src="assets/images/d4.jpg" alt="" class="img-fluid news-image"></a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                    <h4 class="gdnhy-1 mt-4"><a href="#img"></a>
+                    </h4>
+                </div>
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        <a href="#img" class="d-block zoom"><img src="assets/images/rty.jpg" alt="" class="img-fluid news-image"></a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                   
+                </div>
+                <div class="col-xl-2 col-md-4 col-6 product-incfhny mt-4">
+                    <div class="weitemshny-grid oposition-relative">
+                        </a>
+                        <div class="witemshny-inf">
+                        </div>
+                    </div>
+                </div>
+                <op style="margin-left: 22px; font-weight:bold;">THE BYTEMASTER</op>
+                <op style="margin-left: 210px; margin-top: -23px; font-weight:bold;">THE TECHNO TITAN</op>
+                <op style="margin-left: 420px; margin-top: -23px; font-weight:bold;">THE CYBER SLEEK</op>
+                <op style="margin-left: 625px; margin-top: -23px; font-weight:bold;">THE CODE NINJA</op>
+                <op style="margin-left: 820px; margin-top: -23px; font-weight:bold;">THE BYTE BENDER</op>
+            </div>
+            <!--//row-1-->
+
+        </div>
+    </section>
+    <!--//w3l-properties-->
+    <!--/bottom-3-grids-->
+    <!--/footer-9-->
+    <footer class="w3l-footer9">
+        <section class="footer-inner-main py-5">
+            <div class="container py-md-4">
+                <div class="right-side">
+                    <div class="row footer-hny-grids sub-columns">
+                        <div class="col-lg-3 sub-one-left">
+                            <h6>Social </h6>
+                            <!-- <p class="footer-phny pe-lg-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ute dolor sit.</p> -->
+                            <div class="columns-2 mt-4 pt-lg-2">
+                                <ul class="social">
+                                    <li><a href="https://www.facebook.com/computismkolkata/"><span class="fab fa-facebook-f"></span></a>
+                                    </li>
+                                    <li><a href="https://www.linkedin.com/company/computism"><span class="fab fa-linkedin-in"></span></a>
+                                    </li>
+                                    <li><a href="https://twitter.com/computeism"><span class="fab fa-twitter"></span></a>
+                                    </li>
+                                    
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 sub-two-right">
+                        </div>
+                        <div class="col-lg-2 sub-two-right">
+                            
+                            <ul>
+                                <li><a href="about.html"><i class="fas fa-angle-right"></i> About us</a>
+                                </li>
+                                
+                                </li>
+                                <li><a href="goal.html"><i class="fas fa-angle-right"></i>Our Goal
+                                    </a></li>
+                                    <li><a href="credits.html"><i class="fas fa-angle-right"></i> Credits</a>
+                                    </li>
+                                    <li><a href="contact.html"><i class="fas fa-angle-right"></i> Contact us</a>
+                                    </li>
+
+
+
+                            </ul>
+                        </div>
+                       
+                        <div class="col-lg-3 sub-one-left ps-lg-5">
+                            <h6>Stay Update!</h6>
+                            <p class="w3f-para mb-4">Subscribe to our newsletter to receive our weekly feed.</p>
+                            <div class="w3l-subscribe-content align-self mt-lg-0 mt-5">
+                                <form action="#" method="post" class="subscribe-wthree">
+                                    <div class="flex-wrap subscribe-wthree-field">
+                                        <input class="form-control" type="email" placeholder="Email" name="email" required="">
+                                        <button class="btn btn-style btn-primary" type="submit">Subscribe</button>
+                                    </div>
+                                </form>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="below-section mt-5 pt-lg-3">
+                    <div class="copyright-footer">
+                        <div class="columns text-left">
+                            <a href="https://w3layouts.com/" target="_blank">
+                                    </a></p>
+                        </div>
+                        <ul class="footer-w3list text-right">
+                            <li><a href="#url"></a>
+                            </li>
+                            <li><a href="#url"></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <!-- Js scripts -->
+        <!-- move top -->
+        <button onclick="topFunction()" id="movetop" title="Go to top">
+            <span class="fas fa-level-up-alt" aria-hidden="true"></span>
+        </button>
+        <script>
+            // When the user scrolls down 20px from the top of the document, show the button
+            window.onscroll = function() {
+                scrollFunction()
+            };
+
+            function scrollFunction() {
+                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                    document.getElementById("movetop").style.display = "block";
+                } else {
+                    document.getElementById("movetop").style.display = "none";
+                }
+            }
+
+            // When the user clicks on the button, scroll to the top of the document
+            function topFunction() {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+
+        </script>
+        <!-- //move top -->
+    </footer>
+    <!--//footer-9 -->
+
+    <!-- Template JavaScript -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/theme-change.js"></script>
+   
+    <!--/search-->
+    <script src="assets/js/modernizr.custom.js"></script>
+    <script src="assets/js/classie.js"></script>
+    <script src="assets/js/demo1.js"></script>
+    <!--//search-->
+    <!-- MENU-JS -->
+    <script>
+        $(window).on("scroll", function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 80) {
+                $("#site-header").addClass("nav-fixed");
+            } else {
+                $("#site-header").removeClass("nav-fixed");
+            }
+        });
+
+        //Main navigation Active Class Add Remove
+        $(".navbar-toggler").on("click", function() {
+            $("header").toggleClass("active");
+        });
+        $(document).on("ready", function() {
+            if ($(window).width() > 991) {
+                $("header").removeClass("active");
+            }
+            $(window).on("resize", function() {
+                if ($(window).width() > 991) {
+                    $("header").removeClass("active");
+                }
+            });
+        });
+
+    </script>
+    <!-- //MENU-JS -->
+
+    <!-- disable body scroll which navbar is in active -->
+    <script>
+        $(function() {
+            $('.navbar-toggler').click(function() {
+                $('body').toggleClass('noscroll');
+            })
+        });
+
+    </script>
+    <!-- //disable body scroll which navbar is in active -->
+    <!-- //bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ CENTER/cerviceBookings.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+include '../CONNECTION/DbConnection.php';
+include 'centerHead.php';
+$uid = $_SESSION['uid'];
+?>
+
+
+<section class="w3l-blog">
+    <div class="blog py-5" id="Newsblog">
+        <div class="container py-lg-5 py-md-4 py-2">
+            <div class="title-content text-center mb-lg-3 mb-4">
+                <h6 class="title-subw3hny mb-1">Service Bookings</h6>
+                <h3 class="title-w3l mb-5">Service Bookings <br></h3>
+            </div>
+
+            <div class="row justify-content-center">
+                <?php
+                $res = mysqli_query($conn, "SELECT * FROM `tb_service_booking` SB, `user` UR , `tb_service` S WHERE SB.`serviceid`=S.`servicebookid` AND SB.`centerid`='$uid' AND UR.`uid`=SB.`customer_id`");
+                while ($rs = mysqli_fetch_array($res)) {
+                ?>
+                    <div class="col-lg-4 col-md-6 item">
+                        <div class="card">
+                            <div class="card-header p-0 position-relative">
+                                <a href="#blog" class="zoom d-block">
+                                    <!-- <img class="card-img-bottom d-block" src="../assets/image//<?php echo $rs['photo'] ?>" alt="Card image cap"> -->
+                                </a>
+                            </div>
+                            <div class="card-body blog-details">
+
+                                <a href="#blog" class="blog-desc"><?php echo $rs['uname'] ?></a>
+                               
+                                <p> <?php echo $rs['uphoneno'] ?></p>
+                                <p> <?php echo $rs['uaddress'] ?></p>
+                                <hr>
+                                <p> <?php echo $rs['description'] ?></p>
+                                <p>💲 <?php echo $rs['fee'] ?></p>
+                               
+                            </div>
+                            <div class="card-footer">
+                                <div class="author align-items-center">
+                                    <a href="#author" class="post-author">
+                                        <!-- <img src="assets/images/team1.jpg" alt="" class="img-fluid rounded-circle"> -->
+                                    </a>
+                                    <ul class="blog-meta">
+                                        <li>
+                                            <!-- <span class="meta-value"></span><a href="Action.php?sid=<?php echo $rs['servicebookid'] ?>&centerid=<?php echo $centerid ?>&fee=<?php echo $rs['fee'] ?>" class="btn btn-info" style="color: white;"> Book</a> -->
+                                            <!--    <span class="meta-value"></span><a href="StatusAction.php?sid=<?php echo $rs['reg_id'] ?>&status=Rejected" class="btn btn-danger" style="color: white; margin-left:10px;"> Reject</a> -->
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+
+                }
+                ?>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<?php
+include '../COMMON/commonfooter.php';
+?>
